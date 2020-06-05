80e474e3bc84364893f2246710cf3d3d8f143c6b
Colin Twist <ctwist@hotmail.com>
2020-06-04 21:30:05 -0700

    Update script with comments

--- IpAddressesOnly.sh
+++ IpAddressesOnly.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-
+#In this basic script the IpInfo.sh output is piped to sed 
+#sed then uses the -n command to filter out every thing in the output except
+# Those which are IP Address.  This is then Prinited as an output to the screen.
 ./IpInfo.sh | sed -n '/IP Address/p'
     
+
