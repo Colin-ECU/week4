efbea99059c64cd4f638826f342c0d6440eb8972
Colin Twist <ctwist@hotmail.com>
2020-06-12 01:33:18 -0700

    This has an update to GuessingGame of sed replace word to choose/select

--- GuessingGame.sh
+++ GuessingGame.sh
@@ -28,9 +28,9 @@
          
     done
  }
-  
+ # in the below text, Week 5 sed changed 2nd echo from select to choose.
  echo "This is a Guessing Game"
- echo "The rule is you select a number between 1 and 100"
+ echo "The rule is you choose a number between 1 and 100!"
  echo "To Win, you need to pick the Magic number"
  getNumber "please type your guess now" 42 
  echo "YOU WIN!!! Thank you for playing"
